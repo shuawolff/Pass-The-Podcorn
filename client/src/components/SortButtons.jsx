f91b4519152a16c29650bee5bff8f3f17f4578b1
Shua Wolff <shtawo11@gmail.com>
2018-08-22 14:23:51 -0400

    Got filter by genre and search bar working

--- client/src/components/SortButtons.jsx
+++ client/src/components/SortButtons.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function SortButtons(props) {
     return (
         <div>
-            <input value="" name="text-name" type="text" />
-            <button type="button" className="btn podcastSortBtn">Name</button>
-            <button type="button" className="btn podcastSortBtn">Genre</button>
-            <button type="button" className="btn podcastSortBtn">Episodes</button>
+            <input onChange={props.search} value={props.searchBar} name="text-name" type="text" placeholder="Search Genre"/>
+            <button onClick={e => props.filterFunction('Gaming')} type="button" className="btn podcastSortBtn">Gaming</button>
+            <button onClick={e => props.filterFunction('True Crime')} type="button" className="btn podcastSortBtn">True Crime</button>
+            <button onClick={e => props.filterFunction('All')} type="button" className="btn podcastSortBtn">All</button>
             <button type="button" className="btn podcastSortBtn">Likes</button>
             <button type="button" className="btn podcastSortBtn">Dislikes<br/></button>
         </div>
